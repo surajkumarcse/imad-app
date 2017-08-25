43d5914019b99a6dfb3c5712510c18aff4e49cb2
surajkumarcse <30420005+surajkumarcse@users.noreply.github.com>
2017-08-25 16:57:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,6 +104,7 @@ var pool = new Pool(config);
 app.get('/test-db', function (req, res){
    //make a select request
    //return a response with results
+   
    pool.query('SELECT * FROM test', function (err, result){
       if(err){
           res.status(500).send(err.toString());
@@ -131,10 +132,23 @@ app.get('/submit-name', function(req, res){  //URL:/submit-name?name=???
 });
 
 
-app.get('/:articleName', function(req, res){
+app.get('/articles/:articleName', function(req, res) {
     //articlename = article-one
-    var articleName=req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result) {
+       if (err) {
+       res.status(500).send(err.toString());
+       }
+       else {
+           if (result.rows.length === 0)
+           { res.status(404).send('Article not found');
+           }
+           else {
+               var articleData = result.rows[0];
+               res.send(createTemplate(articleData));
+           }
+       }
+    });
+    
 });
 
 app.get('/ui/style.css', function (req, res) {
