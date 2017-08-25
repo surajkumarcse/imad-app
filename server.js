d14e3797ba96c2505f27e4069ae62a23e853cf80
surajkumarcse <30420005+surajkumarcse@users.noreply.github.com>
2017-08-25 16:20:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/test-db', function (req, res){
           
       } 
       else {
-          res.send(JSON.stringify(result));
+          res.send(JSON.stringify(result.rows));
       }
       
    });
