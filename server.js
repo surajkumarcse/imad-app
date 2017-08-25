27d1077a279c0e7149f6faa4fe32cf6056823c8d
surajkumarcse <30420005+surajkumarcse@users.noreply.github.com>
2017-08-25 15:52:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+    user: 'surajkumar2cse20', 
+    database: 'surajkumar2cse20',
+    port: '5432', 
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -92,7 +99,21 @@ function createTemplate (data)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var pool = new Pool(config);
+app.get('/test-db', function (req, res){
+   //make a select request
+   //return a response with results
+   pool.query('SELECT * FROM test', function(err, result){
+      if(err){
+          res.status(500).send(err.toString());
+          
+      } 
+      else {
+          res.send(JSON.stringify(result));
+      }
+      
+   });
+});
 var counter=0;
 app.get('/counter', function (req, res){
     counter = counter + 1;
