8e89101b8b25a50e31995633c853929043d72a35
surajkumarcse <30420005+surajkumarcse@users.noreply.github.com>
2017-09-02 03:43:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,37 +1,4 @@
 //counter code
-var button = document.getElementById('counter');
-
-button.onclick = function (){
-    //create the request object
-    var request= new XMLHttpRequest();
-    
-    //capture the response and store it in variable
-    request.onreadystatechange = function(){
-      if (request.readyState === XMLHttpRequest.DONE) {
-         if (request.status === 200){
-               var names = ['name1', 'name2', 'name3', 'name4'];
-               
-              var list = '';
-            for (var i=0; i < names.length; i++){
-                list += '<li>' + name[i] + '</li>';
-            }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list;
-   
-         } 
-      }
-      request.open('GET', 'http://surajkumar2cse20.imad.hasura-app.io/counter' , true);
-    request.send(null);
-
-      //not done yet
-    };
-    
-        //make the request
-    
-    //submit name
-    
-    
-};
 
 
 //submit name
@@ -48,24 +15,28 @@ submit.onclick = function(){
          if (request.status === 200){
              
                               
-               var names = request.responseText;
-               names = JSON.parse(names);
-              var list = '';
-            for (var i=0; i < names.length; i++){
-                list += '<li>' + name[i] + '</li>';
- 
-            }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list;
+               console.log('user logged in');
+               alert('logged in successfully');
    
-         } 
+         } else if (request.status === 403) {
+             alert('username/password is not correct');
+             
+         }
+         else if  (request.status === 500) {
+             alert('something went wrong');
+         }
+         
+         
       } 
       //not done yet
     };
-    var nameInput = document.getElementById('name');
-     var name = nameInput.value;
-    request.open('GET', 'http://surajkumar2cse20.imad.hasura-app.io/submit-name?name=' + name , true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+      console.log(username);
+      console.log (password);
+    request.open('POST', 'http://surajkumar2cse20.imad.hasura-app.io/login' , true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password})) ;
     //make the request
     
     //submit name
